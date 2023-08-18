402d727322db30763f00084f12dfacceec8d5385
kevin <kevin.galaxy04@gmail.com>
2023-08-18 12:16:49 -0300

    production

--- node/app.js
+++ node/app.js
@@ -1,17 +1,15 @@
 import express  from "express";
 import cors  from 'cors'
-import dotenv from 'dotenv'
 import db from "./database/db.js";
 import blogRoutes from './routes/routes.js'
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/',"Hola")
 app.use( cors())
 app.use(express.json())
 app.use('/blogs', blogRoutes)
-dotenv.config({path:'./.env'})
+
 
 app.listen(port, ()=>{
     console.log(`Server UP running in http://localhost:${port}/`)
--- node/database/db.js
+++ node/database/db.js
@@ -1,6 +1,4 @@
 import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv'
-dotenv.config({path:'./.env'})
 
 const db= new Sequelize(process.env.DB_DATABASE, process.env.DB_USER,process.env.DB_PASS,{
     host: process.env.DB_HOST,
