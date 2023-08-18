56cc90fd826adeb6953b9ea929a842fff2840956
kevin <kevin.galaxy04@gmail.com>
2023-08-18 12:12:00 -0300

    production

--- node/app.js
+++ node/app.js
@@ -7,6 +7,7 @@ import blogRoutes from './routes/routes.js'
 const app = express();
 const port = process.env.PORT;
 
+app.get('/',"Hola")
 app.use( cors())
 app.use(express.json())
 app.use('/blogs', blogRoutes)
