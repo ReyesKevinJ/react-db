7b47e669d937795871710aab314401174e7a2ff3
kevin <kevin.galaxy04@gmail.com>
2023-08-18 13:10:19 -0300

    conection

--- reactfront/src/blog/CreateBlog.jsx
+++ reactfront/src/blog/CreateBlog.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useState } from 'react'
 import {useNavigate} from 'react-router-dom'
 
-const URI = 'https://react-db-production.up.railway.app/blogs'
+const URI = 'https://react-db-production.up.railway.app/blogs/create'
 
 const CompCreateBlog = () =>{
     const [title, setTitle]= useState('')
