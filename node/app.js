452f2128d32b7dba3144bd5634212ea3bb9b847a
kevin <kevin.galaxy04@gmail.com>
2023-08-18 11:56:15 -0300

    production

--- node/app.js
+++ node/app.js
@@ -5,19 +5,12 @@ import db from "./database/db.js";
 import blogRoutes from './routes/routes.js'
 
 const app = express();
+const port = process.env.PORT;
+
 app.use( cors())
 app.use(express.json())
 app.use('/blogs', blogRoutes)
 dotenv.config({path:'./.env'})
-
-try {
-    await db.authenticate()
-    console.log('Conexion Exitosa')
-} catch (error) {
-    console.log(`Error: ${error}`)
-}
-const port = process.env.PORT;
-
 
 app.listen(port, ()=>{
     console.log(`Server UP running in http://localhost:${port}/`)
