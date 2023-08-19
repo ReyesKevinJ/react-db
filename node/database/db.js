c4fdcf94115b20056c02c600b9b386aed9fae099
kevin <kevin.galaxy04@gmail.com>
2023-08-19 00:07:53 -0300

    conection

--- node/database/db.js
+++ node/database/db.js
@@ -4,10 +4,12 @@ import {
     DB_HOST,
     DB_PASS,
     DB_USER,
+    DB_PORT
  } from '../config.js';
 const db= new Sequelize(DB_DATABASE, DB_USER,DB_PASS,{
     host: DB_HOST,
     dialect: 'mysql',
+    port: DB_PORT
 })
 
 
